034bb3ed81476f85575564902783c70211005173
ben <phas0ruk@gmail.com>
2022-09-21 21:16:22 -0700

    adds styles to nav user app

--- frontend-user/src/components/Header.js
+++ frontend-user/src/components/Header.js
@@ -4,11 +4,13 @@ import {Link} from 'react-router-dom';
 export default function Header({isLoggedIn}) {
 
   return (
-    <div className='w-full h-[90px] border-b border-gray-100 fixed top-0 z-50 drop-shadow-xl'>
-    <Link to='/'><h1>Ben's Blog</h1></Link> 
-        <div>
-            {isLoggedIn ? <Link to="/logout">Log Out</Link> : <Link to="/login">Log In</Link>}
-            {!isLoggedIn && <Link to='/signup'>Sign Up</Link>}
+    <div className='w-full h-[90px] border-b border-gray-100 fixed top-0 z-50 drop-shadow-xl flex justify-center bg-slate-50'>
+        <div className='w-full p-6 ml-20 mr-20 flex justify-between'>
+            <Link to='/'><h1 className="text-4xl font-semibold hover:border-b hover:border-gray-500">Ben's Blog</h1></Link> 
+            <div className='p-2'>
+                {isLoggedIn ? <Link to="/logout" className="mr-6 border-b border-gray-300 hover:border-gray-500">Log Out</Link> : <Link to="/login" className="mr-6 border-b border-gray-300 hover:border-gray-500">Log In</Link>}
+                {!isLoggedIn && <Link to='/signup' className='border-b border-gray-300 hover:border-gray-500'>Sign Up</Link>}
+            </div>
         </div>
     </div>
   )
